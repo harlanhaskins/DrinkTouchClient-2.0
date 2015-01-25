081748df0d5661597bf5db420274fb8af14b232d
Harlan Haskins <harlan@harlanhaskins.com>
2015-01-25 00:57:38 -0500

    Modifies browser_controller to be more modular

---
+++ backend/browser.py
@@ -0,0 +1,20 @@
+import requests
+from selenium import webdriver
+
+browser = None
+
+def open_url(url):
+    global browser
+    if not browser:
+        browser = new_browser()
+    browser.get(url)
+
+def new_browser():
+    return webdriver.Firefox()
+
+if __name__ == "__main__":
+    import time
+
+    while True:
+        time.sleep(4)
+        open_url("http://google.com")
