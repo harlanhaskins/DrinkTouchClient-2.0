57f3fa072a5ee1466589230b908e2978a01dfb00
Harlan Haskins <harlan@harlanhaskins.com>
2015-01-25 15:09:24 -0500

    Added configuration and initial serial implementation to ibutton.py

--- ibutton.py
+++ ibutton.py
@@ -2,29 +2,39 @@
 
 import browser
 import serial
+import json
 from User import User
 
 #Serial connection to ibutton reader
-ser = None
+ibutton_serial = None
+rfid_serial = None
+
 
 #User object
 user = None
 
+def read_config():
+    with open('config.json') as config_file:
+        return json.load(cofig_file)
 
 """
 Initializes the serial connection on the first avalaible serial connection.
 
 If no serial connection is avalaible an exception is thrown
 """
-def init_serial():
-    pass
+def init_serial(ibuton_address, rfid_address):
+    # TODO: Read from config file.
+    ibutton_serial = serial.Serial(ibutton_address)
+    rfid_serial = serial.Serial(rfid_address)
 
-def read_ibutton():
-    # TODO: Replace with reading from serial.
-    with open("ibutton.txt") as ibutton_file:
-        return ibutton_file.readline().strip()
+def read_ibutton(debug=False):
+    if debug:
+        with open("ibutton.txt") as ibutton_file:
+            return ibutton_file.readline().strip()
 
 def main():
+    config = read_config()
+    init_serial(config['ibutton_address'], config['rfid_address'])
     ibutton_id = read_ibutton()
     user = User(ibutton_id)
     browser.open_url("http://webdrink.csh.rit.edu/kiosk/" + user.username)
