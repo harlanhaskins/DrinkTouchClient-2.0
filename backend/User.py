697c800f3422a0bb41300b56844a47f8f7dd34cd
Harlan Haskins <harlan@harlanhaskins.com>
2015-01-25 12:45:46 -0500

    Removed frontend, cleaned up User, and added the correct behavior.

--- backend/User.py
+++ backend/User.py
@@ -1,25 +1,36 @@
 import requests
 
-class user:
-    username = None
-    drinkCredits = None
-    displayName = None
-    ibuttonId = None
-    drinkAdmin = False
+BASE_URL = "http://csh.rit.edu:56124/"
 
+class User(object):
+
+    __slots__ = ('username', 'drink_credits', 'display_name',
+            'ibutton_id', 'is_drink_admin')
 
     """
-    initializes object and instance variables based on ibuttonId
+    Initializes object and instance variables based on ibuttonId
 
     Makes a request to csh.rit.edu:56124/?ibutton and gets user information
     """
-    def __init__(self, ibuttonId):
-        print("New User Object:" + ibuttonId)
+    def __init__(self, ibutton_id):
+        r = requests.get(BASE_URL, params={"ibutton": ibutton_id})
 
-        self.username = "testuser"
-        self.drinkCredits = 1
-        selfdisplayName = "Test User"
-        self.ibuttonId = ibuttonId
-        self.drinkAdmin = False
+        user = r.json()
 
+        self.username = user['uid']
+        self.drink_credits = user['credits']
+        self.display_name = user['cn']
+        self.ibutton_id = ibutton_id
+        self.is_drink_admin = user['drinkAdmin']
 
+        print(self)
+
+    def __repr__(self):
+        string = self.display_name
+        string += "\n  "
+        string += "username: " + self.username
+        string += "\n  "
+        string += "credits: " + str(self.drink_credits)
+        string += "\n  "
+        string += "admin: " + str(self.is_drink_admin)
+        return string
--- backend/ibutton.py
+++ backend/ibutton.py
@@ -2,7 +2,7 @@
 
 import browser
 import serial
-import User
+from User import User
 
 #Serial connection to ibutton reader
 ser = None
@@ -19,14 +19,16 @@ If no serial connection is avalaible an exception is thrown
 def init_serial():
     pass
 
-
-
+def read_ibutton():
+    # TODO: Replace with reading from serial.
+    with open("ibutton.txt") as ibutton_file:
+        return ibutton_file.readline().strip()
 
 def main():
-    #ibuttonId = ser.readline()
-    ibuttonId = "123"
-    user = User.user(ibuttonId)
-    browser.open_url("http://google.com/"+user.username)
+    #ibutton_id = ser.readline()
+    ibutton_id = read_ibutton()
+    user = User(ibutton_id)
+    browser.open_url("http://google.com/" + user.username)
 
 if __name__ == "__main__":
     main()
