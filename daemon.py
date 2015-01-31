85768354703b53d0375c028b0099e6b2340f750e
Harlan Haskins <harlan@harlanhaskins.com>
2015-01-31 22:03:33 +0000

    Added reimplementation of browser.browse() that takes javascript into account

--- daemon.py
+++ daemon.py
@@ -1,13 +1,17 @@
-#TODO: Implement daemon
 import json
 import argparse
 import spynner
+import time
 from ibutton import iButton
-from multiprocessing import Pool
+from threading import Thread, Lock
 
-browser = spynner.Browser(debug_level=spynner.DEBUG)
+browser = spynner.Browser()
 browser.create_webview()
-browser.webview.showFullScreen()
+browser.show()
+# browser.webview.showFullScreen()
+
+js_to_run = None
+js_lock = Lock()
 
 def read_config():
     with open('config.json') as config_file:
@@ -17,17 +21,33 @@ def main(debug=False, verbose=False):
     config = read_config()
     browser.load("https://webdrink.csh.rit.edu/touchscreen/?machine_id=%d" %
             config['machine_id'])
-    p = Process(target=start_reading)
+    p = Thread(target=start_reading, args=(config, browser, debug,))
     p.start()
+    browse()
 
-def start_reading():
+def browse():
+    global js_to_run
+    while True:
+        if js_to_run:
+            with js_lock:
+                print("running %s" % js_to_run)
+                browser.runjs(js_to_run)
+                js_to_run = None
+        browser._events_loop()
+
+def run_js(js):
+    global js_to_run
+    with js_lock:
+        js_to_run = js
+
+def start_reading(config, browser, debug):
     while(True):
         ibutton = iButton(config['ibutton_address'], config['rfid_address'],
                         debug=debug)
         print("reading...")
         ibutton_id = ibutton.read()
         print("found ibutton: '%s'" % ibutton_id)
-        browser.runjs("app.loadiButton(\"%s\");" % ibutton_id)
+        run_js("app.loadiButton(\"%s\");" % ibutton_id)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
